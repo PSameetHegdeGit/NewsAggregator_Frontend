b595efd196f2d2a4ba2e4b12483dfe86d637d31e
PSameetHegdeGit <sameethegde123@gmail.com>
2020-08-20 19:15:09 -0400

    updated server.js again

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/frontend'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/frontend/index.html'));});
+'/dist/frontend/index.html'));});
 app.listen(process.env.PORT || 8080);
